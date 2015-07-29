60acfdbf1ee4870e7687d3c3368edf3c07c3b6ac
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-29 09:58:40 +0100

    add token for github api and pass tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -20,6 +20,7 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'js/**/*.js',
+      'js/token.js',
       'test/**/*.spec.js'
     ],
 
